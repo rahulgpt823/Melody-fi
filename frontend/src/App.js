d3292899886ceae038af27b953deadd6e811d33c
Rahul Gupta <rahul@kuttl.in>
2024-04-28 16:30:15 +0530

    tailwind CSS setup along with React router dom library setup to switch between the different routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Routes,
+  Route,
+  Link,
+  BrowserRouter,
+} from "react-router-dom";
+// Beowser router act as a wrapper so all the route we need to add shuld go inside it 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <BrowserRouter>
+      <Routes>
+        {/*Adding route component here indicaticates to the package(react-router-dom) that */}
+        <Route>
+          {/* Here you define the single routes format: <Route path='/' element={<Home />} />*/}
+          
+        </Route>
+        <Route path="/" element ={<div>This is the main page.</div>} />
+        <Route path="/home" element ={<HomeComponent />} />
+        </Routes>
+      
+      </ BrowserRouter> 
+
+
     </div>
   );
-}
 
+}
+ const HomeComponent=()=>{ // funct
+  
+  return <div>This is the home page </div>;
+ };
 export default App;
